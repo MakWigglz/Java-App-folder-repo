57506e21c695f77b08462f9484c75ff2bf2a1a11
MakWigglz <wigglz734@gmail.com>
2024-08-30 07:06:18 +0300

    chore: Add necessary files and configuration for a Java web app

--- WebApp.java
+++ WebApp.java
@@ -2,25 +2,26 @@ import java.util.Scanner;
 
 public class WebApp {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        UserPrompt userPrompt = new UserPrompt();
-        ResponseEvaluator evaluator = new ResponseEvaluator();
+        try (Scanner scanner = new Scanner(System.in)) {
+            UserPrompt userPrompt = new UserPrompt();
+            ResponseEvaluator evaluator = new ResponseEvaluator();
 
-        System.out.println("Are you male? (true/false)");
-        userPrompt.setIsMale(scanner.nextBoolean());
+            System.out.println("Are you male? (true/false)");
+            userPrompt.setIsMale(scanner.nextBoolean());
 
-        System.out.println("Are you employed? (true/false)");
-        userPrompt.setIsEmployed(scanner.nextBoolean());
+            System.out.println("Are you employed? (true/false)");
+            userPrompt.setIsEmployed(scanner.nextBoolean());
 
-        System.out.println("Are you younger than 40? (true/false)");
-        userPrompt.setIsYoungerThan40(scanner.nextBoolean());
+            System.out.println("Are you younger than 40? (true/false)");
+            userPrompt.setIsYoungerThan40(scanner.nextBoolean());
 
-        System.out.println("Do you like coding? (true/false)");
-        userPrompt.setLikesCoding(scanner.nextBoolean());
+            System.out.println("Do you like coding? (true/false)");
+            userPrompt.setLikesCoding(scanner.nextBoolean());
 
-        boolean[] responses = userPrompt.getUserResponses();
-        String result = evaluator.evaluateResponses(responses);
+            boolean[] responses = userPrompt.getUserResponses();
+            String result = evaluator.evaluateResponses(responses);
 
-        System.out.println(result);
+            System.out.println(result);
+        }
     }
 }
